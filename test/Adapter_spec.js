bf3aad2ece19af0e135a4d4a7f178decdc7be492
Jeff Jagoda <jjagoda@bandwidth.com>
2014-10-06 12:18:40 -0400

    Adapter broadcast.

--- test/Adapter_spec.js
+++ test/Adapter_spec.js
@@ -1,17 +1,27 @@
 "use strict";
-var Adapter = require("../lib/Adapter");
-var expect  = require("chai").expect;
-var UUID    = require("node-uuid");
+var Adapter      = require("../lib/Adapter");
+var ClientSocket = require("../lib/ClientSocket");
+var expect       = require("chai").expect;
+var Namespace    = require("../lib/Namespace");
+var Server       = require("../lib/Server");
+var sinon        = require("sinon");
+var UUID         = require("node-uuid");
 
 describe("An adapter", function () {
 	var adapter;
+	var namespace;
 
 	before(function () {
-		adapter = new Adapter();
+		namespace = new Namespace(new Server(), "/");
+		adapter   = namespace.adapter = new Adapter(namespace);
 	});
 
 	it("has a hash of rooms", function () {
 		expect(adapter, "rooms").to.have.property("rooms").that.deep.equal({});
+	});
+
+	it("has a namespace", function () {
+		expect(adapter, "namespace").to.have.property("nsp", namespace);
 	});
 
 	describe("adding a socket to a new room", function () {
@@ -65,6 +75,73 @@ describe("An adapter", function () {
 			it("removes the room entry", function () {
 				expect(adapter.rooms[room], "socket").not.to.have.property(secondId);
 			});
+		});
+	});
+
+	describe("broadcasting a message", function () {
+		var handlers = [];
+		var message  = "a test message";
+		var room     = "foo";
+
+		var clients;
+		var servers;
+
+		before(function () {
+			clients = [ null, null, null, null ].map(function () {
+				var client  = new ClientSocket();
+				var handler = sinon.spy();
+
+				client.once("message", handler);
+				handlers.push(handler);
+				return client;
+			});
+
+			servers = clients.map(function (client, index) {
+				var socket = namespace.add(client);
+
+				if (index < 3) {
+					socket.join(room);
+				}
+				return socket;
+			});
+
+			adapter.broadcast(
+				{
+					data : [
+						"message",
+						message
+					]
+				},
+				{
+					rooms  : [ room ],
+					except : servers[2].id
+				}
+			);
+		});
+
+		after(function () {
+			servers.forEach(function (socket, index) {
+				if (index < 3) {
+					socket.leave(room);
+				}
+				namespace.remove(socket);
+			});
+		});
+
+		it("does not send the message to sockets not in the room", function () {
+			expect(handlers[3].callCount, "message").to.equal(0);
+		});
+
+		it("does not send the message to sockets on the ignore list", function () {
+			expect(handlers[2].callCount, "message").to.equal(0);
+		});
+
+		it("sends the message to all matching sockets", function () {
+			expect(handlers[0].callCount, "message event").to.equal(1);
+			expect(handlers[0].firstCall.args[0], "message").to.equal(message);
+
+			expect(handlers[1].callCount, "message event").to.equal(1);
+			expect(handlers[1].firstCall.args[0], "message").to.equal(message);
 		});
 	});
 });
