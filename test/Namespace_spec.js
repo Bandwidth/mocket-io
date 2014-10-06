d1e0778e6a7f67c361b5397b48ec5faa749429e0
Jeff Jagoda <jjagoda@bandwidth.com>
2014-10-06 13:19:39 -0400

    Middleware support.

--- test/Namespace_spec.js
+++ test/Namespace_spec.js
@@ -5,6 +5,7 @@ var expect       = require("chai").expect;
 var Namespace    = require("../lib/Namespace");
 var Server       = require("../lib/Server");
 var ServerSocket = require("../lib/ServerSocket");
+var sinon        = require("sinon");
 
 describe("A namespace", function () {
 	var namespace;
@@ -25,7 +26,7 @@ describe("A namespace", function () {
 
 	it("has a list of sockets", function () {
 		expect(namespace, "sockets").to.have.property("sockets")
-		.that.deep.equal([]);
+		.that.deep.equals([]);
 	});
 
 	it("has an adapter", function () {
@@ -33,13 +34,24 @@ describe("A namespace", function () {
 		.that.is.an.instanceOf(Adapter);
 	});
 
+	it("has a stack of middleware", function () {
+		expect(namespace, "middleware").to.have.property("fns")
+		.that.deep.equals([]);
+	});
+
 	describe("adding a client socket", function () {
+		var run;
 		var socket;
 
 		before(function () {
 			var client = new ClientSocket();
 
+			run    = sinon.spy(namespace, "run");
 			socket = namespace.add(client);
+		});
+
+		after(function () {
+			run.restore();
 		});
 
 		it("returns a server socket", function () {
@@ -48,6 +60,10 @@ describe("A namespace", function () {
 
 		it("augments the socket list", function () {
 			expect(namespace.sockets, "socket list").to.include(socket);
+		});
+
+		it("runs the middleware on the socket", function () {
+			expect(run.callCount, "run").to.equal(1);
 		});
 
 		describe("and then removing it", function () {
@@ -71,6 +87,46 @@ describe("A namespace", function () {
 					expect(namespace.sockets, "length").to.have.length(sockets);
 				});
 			});
+		});
+	});
+});
+
+describe("A namespace configured with middleware", function () {
+	var namespace;
+	var socket;
+
+	function middleware1 (socket, next) {
+		socket.data = socket.data || [];
+		socket.data.push("foo");
+		next();
+	}
+
+	function middleware2 (socket, next) {
+		socket.data = socket.data || [];
+		socket.data.push("bar");
+		next();
+	}
+
+	before(function () {
+		namespace = new Namespace(new Server(), "/");
+		namespace.use(middleware1);
+		namespace.use(middleware2);
+
+		socket = new ServerSocket(namespace);
+	});
+
+	it("has a list of middleware", function () {
+		expect(namespace, "middleware").to.have.property("fns")
+		.that.deep.equals([ middleware1, middleware2 ]);
+	});
+
+	describe("running the middleware", function () {
+		before(function () {
+			namespace.run(socket);
+		});
+
+		it("applies the middleware to new sockets in order", function () {
+			expect(socket.data, "data").to.deep.equal([ "foo", "bar" ]);
 		});
 	});
 });
--- test/Server_spec.js
+++ test/Server_spec.js
@@ -2,6 +2,7 @@
 var ClientSocket = require("../lib/ClientSocket");
 var EventEmitter = require("events").EventEmitter;
 var expect       = require("chai").expect;
+var Namespace    = require("../lib/Namespace");
 var Server       = require("../lib/Server");
 var ServerSocket = require("../lib/ServerSocket");
 var sinon        = require("sinon");
@@ -16,6 +17,12 @@ describe("A Server", function () {
 	it("is an event emitter", function () {
 		expect(server, "emitter").to.be.an.instanceOf(EventEmitter);
 		expect(server, "constructor").to.have.property("constructor", Server);
+	});
+
+	it("has a root namespace", function () {
+		expect(server, "root namespace").to.have.property("sockets")
+		.that.is.an.instanceOf(Namespace)
+		.and.that.has.property("name", "/");
 	});
 
 	describe("creating a client socket", function () {
@@ -40,6 +47,26 @@ describe("A Server", function () {
 		it("emits a server socket", function () {
 			expect(connection.callCount, "connection event").to.equal(1);
 			expect(connection.firstCall.args[0], "socket").to.be.an.instanceOf(ServerSocket);
+		});
+	});
+
+	describe("adding middleware", function () {
+		var middleware = function () {};
+
+		var use;
+
+		before(function () {
+			use = sinon.stub(server.sockets, "use");
+			server.use(middleware);
+		});
+
+		after(function () {
+			use.restore();
+		});
+
+		it("places it on the root namespace", function () {
+			expect(use.callCount, "use").to.equal(1);
+			expect(use.firstCall.args[0], "middleware").to.equal(middleware);
 		});
 	});
 });
