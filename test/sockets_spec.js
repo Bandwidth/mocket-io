229cb7657a2704fdbe5327f03e8342cc9e898549
Jeff Jagoda <jjagoda@bandwidth.com>
2014-10-06 14:00:09 -0400

    Remap top-level API.

--- test/sockets_spec.js
+++ test/sockets_spec.js
@@ -1,8 +1,26 @@
 "use strict";
 var Client = require("../lib/Client");
 var expect = require("chai").expect;
+var fs     = require("fs");
+var path   = require("path");
 var Server = require("../lib/Server");
 var sinon  = require("sinon");
+
+describe("The top-level API", function () {
+	var api = require("..");
+
+	it("exposes all modules", function () {
+		var keys = fs.readdirSync(path.join(__dirname, "..", "lib"))
+		.filter(function (key) {
+			return key !== "index.js";
+		})
+		.map(function (key) {
+			return path.basename(key, ".js");
+		});
+
+		expect(api, "api").to.have.keys(keys);
+	});
+});
 
 describe("A socket connection", function () {
 	var clientSocket;
